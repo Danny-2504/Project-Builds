6b175c5c369bbb24f4c827f9264c81da1e715263
Danny-2504 <macbookshreyash@gmail.com>
2023-08-23 10:25:24 +0530

    Wednesday morning push

---
+++ Build-6/wed-react/src/pages/user/Orders.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { BASE_API_URL } from "../../common/constant";
+import orderService from "../../service/order.service";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const Orders = () => {
+  const [orderList, setOrderList] = useState([]);
+  const [orderContent,setOrderContent] = useState("AllOrders") ;
+  const navigate = useNavigate();
+
+ const user= useSelector((u)=>u.user)
+
+  useEffect(() => {
+    orderService
+      .getOrderByUser(user.id)
+      .then((res) => {
+        setOrderList(res.data);
+        console.log(res.data)
+      })
+      .catch((error) => {
+        console.log(error);
+        navigate('/')
+        
+      });
+  }, []);
+
+  return (
+    
+    <div >
+      <div className="container mt-10" style={{marginTop:'20px'}}>
+      <div className="card paint-card">
+        <div className="card-body">
+       
+        <h4 className="form-signin-heading text-center">Order Details</h4>
+        <div className="container-fluid">
+        <div style={{textAlign:'right'}}>
+        <button type="button" className="btn btn-primary mr-2" style={{ width: '150px', }} onClick={()=>{ setOrderContent("AllOrders")}}>All Orders</button>
+        <button type="button" className="btn btn-danger mx-2" style={{ width: '150px', alignContent:'flex-end',alignContent:'flex-end'}}  onClick={()=>{setOrderContent("Order Recieved")}} >Pending Orders</button>
+        </div>
+        </div>
+        
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Image</th>
+                <th scope="col">Order Id</th>
+                <th scope="col">Book Details</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Price</th>
+                <th scope="col">Payment Type</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orderList.map((item, ind) => {
+                if(orderContent=="AllOrders" || item.status == orderContent)
+                return(<tr key={item.id}>
+                  <th scope="row">
+                    <img
+                      src={BASE_API_URL + "/" + item.book.img}
+                      width="70px"
+                      height="70px"
+                    />
+                  </th>
+
+                  <th scope="row">{item.orderNumber}</th>
+
+                  <td>
+                    {item.book.bookName}
+                    <br />
+                    Author: {item.book.author}
+                    <br />
+                    ISBN :{item.book.isbnNo} 
+                  
+                  </td>
+
+                  <td>{item.quantity}</td>
+
+                  <td>{item.price}</td>
+                  <td>{item.paymentType}</td>
+                  <td >{item.status}</td>
+                </tr>)
+                
+})}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
---
+++ Build-6/wed-react/src/service/order.service.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { BASE_API_URL } from "../common/constant";
+import { AuthHeader } from "./auth.header";
+
+const API_URL = BASE_API_URL + "/api/user"
+
+class OrderService {
+
+    createOrder(type,id) {
+        return axios.get(API_URL + "/order/" + type+"/"+id, { headers: AuthHeader() });
+    }
+
+    getOrderByUser(id) {
+        return axios.get(API_URL + "/order/"+id, { headers: AuthHeader() });
+    }
+
+    getAllOrder() {
+        return axios.get(BASE_API_URL + "/api/admin/orders",{ headers: AuthHeader() });
+    }
+
+    updateOrder(id, st) {
+        return axios.put(BASE_API_URL + "/api/admin/updatestatus/"+ id + "/" + st,{ headers: AuthHeader() });
+    }
+
+
+}
+
+export default new OrderService();
